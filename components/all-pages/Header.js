3c102752a30cbb1bab6a908eeb1851e889d4047f
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2020-07-11 12:24:29 -0300

    Navbar Fixed

--- components/all-pages/Header.js
+++ components/all-pages/Header.js
@@ -17,36 +17,38 @@ import DropdownItemToGo from './DropdownItemToGo'
 const Header = (props) => {
     const [isOpen, setIsOpen] = useState(false);
     const toggle = () => setIsOpen(!isOpen);
+    const sizeToggle = "md";
 
     return (
         <div>
-            <Navbar color="light" light expand="md">
-                <NavbarBrand href="/">Pedro Programmer</NavbarBrand>
-                <NavbarToggler onClick={toggle} className="align-content-end mr-1 d-flex" />
+            <div>
+                <Navbar color="light" light expand="md">
+                    <NavbarBrand href="/">Pedro Programmer</NavbarBrand>
+                    <NavbarToggler onClick={toggle} />
+                    <Collapse isOpen={isOpen} navbar className="d-flex justify-content-end">
+                        <Nav className={`${!isOpen ? 'd-none' : ''} d-${sizeToggle}-flex`} navbar>
+                            <AdressToGo adress="/" name="Home" />
+                            <AdressToGo adress="/about" name="About" />
+                            <AdressToGo adress="/contact" name="Contact" />
+                            <UncontrolledDropdown nav inNavbar>
+                                <DropdownToggle nav caret className="text-dark"> Dev Tips </DropdownToggle>
+                                <DropdownMenu right>
+                                    <DropdownItemToGo adress="code-tips/bootstrap" name="Bootstrap" />
+                                    <DropdownItemToGo adress="code-tips/reactstrap" name="ReactStrap" />
+                                    <DropdownItemToGo adress="code-tips/css" name="CSS" />
+                                    <DropdownItem divider />
+                                    <DropdownItemToGo adress="code-tips/javascript" name="JavaScript" />
+                                    <DropdownItemToGo adress="code-tips/vs-code" name="VS Code" />
+                                    <DropdownItem divider />
+                                    <DropdownItemToGo adress="code-tips/web-api" name="Web API" />
 
-                <Collapse isOpen={isOpen} navbar>
-                    <Nav className="mr-auto" navbar>
-                        <AdressToGo adress="/" name="Home" />
-                        <AdressToGo adress="/about" name="About" />
-                        <AdressToGo adress="/blog" name="Blog" />
-                        <UncontrolledDropdown nav inNavbar>
-                            <DropdownToggle nav caret> Dev Tips </DropdownToggle>
-                            <DropdownMenu right>
-                                <DropdownItemToGo adress="code-tips/react" name="React" />
-                                <DropdownItem divider />
-                                <DropdownItemToGo adress="code-tips/bootstrap" name="Bootstrap" />
-                                <DropdownItemToGo adress="code-tips/reactstrap" name="ReactStrap" />
-                                <DropdownItemToGo adress="code-tips/css" name="CSS" />
-                                <DropdownItem divider />
-                                <DropdownItemToGo adress="code-tips/javascript" name="JavaScript" />
-                                <DropdownItemToGo adress="code-tips/vs-code" name="VS Code" />
-                                <DropdownItem divider />
-                                <DropdownItemToGo adress="code-tips/web-api" name="Web API" />
-                            </DropdownMenu>
-                        </UncontrolledDropdown>
-                    </Nav>
-                </Collapse>
-            </Navbar>
+                                </DropdownMenu>
+                            </UncontrolledDropdown>
+                        </Nav>
+
+                    </Collapse>
+                </Navbar>
+            </div>
         </div>
     );
 }
