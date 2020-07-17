31847650ac696c95d4da3a65760c0068e68fa872
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2020-07-16 23:52:32 -0300

    JS tips

--- data/code-tips/javascript.js
+++ data/code-tips/javascript.js
@@ -8,7 +8,13 @@ const CODE_TIPS_DATA = {
     {
         title: "Check if an string is part of another string",
         content: "searched.includes(search)"
-    }
+    },
+
+    {
+        title: "Check if an object has one specific property",
+        content: "obj.hasOwnProperty(prop) - more info in https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/HasOwnProperty"
+    },
 ]}
 
 export default CODE_TIPS_DATA;
+
