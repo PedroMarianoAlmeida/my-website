0ff74be661e18329316fb2bbf5f73806b14fa280
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2020-09-05 22:53:22 -0300

    Insert more host images

--- pages/images.js
+++ pages/images.js
@@ -2,6 +2,8 @@ const Images = () => {
     return (
         <div>
             <img src='/images/photo-host/CN_Tower.PNG' />
+            <img src='/images/photo-host/niagara_falls.PNG' />
+            <img src='/images/photo-host/St_Lawrence_Market.PNG' />
         </div>
       );
 }
