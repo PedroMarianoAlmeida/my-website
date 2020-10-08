7c9f93c730e670981ca62ea261eb743a5a0630e3
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2020-10-08 20:41:52 -0300

    Steps to run Strapi on Heroku

--- data/code-tips/web-tools.js
+++ data/code-tips/web-tools.js
@@ -69,6 +69,69 @@ const CODE_TIPS_DATA = {
         title: "Start Strapi server",
         content: "npm run strapi develop"
     },  
+
+    {
+        title: "Steps to run Strapi on Heroku",
+        content: {
+            __html:
+                `<ol>
+                    <li>                        
+                        Prepare PostgreSQL server
+                            <ol>
+                                <li>
+                                    Open pgAdmin4 on computer
+                                    <ul>
+                                        Notes:
+                                        <li>If receive an error, open the browser in http://127.0.0.1:53039/browser/</li>
+                                        <li>It should open a tab on browser with pgAdmin postgree</li>
+                                    </ul>
+                                </li>
+
+                                <li>Create a new database (or use an existing one)</li>
+                                <li>Create a new user (or use an existing one) </li>
+                            </ol>
+                    </li>
+
+                    <li>
+                        Follow this steps: https://tute.io/install-configure-strapi-postgresql
+                        <ul>Notes:
+                            <li>Except the postgree CLI steps (you already create on steps above)</li>
+                            <li>To create, use the red Git Bash (the yellow doens't allow change the options using the arrows)</li>
+                        </ul>
+                    </li>
+                                
+                    <li>
+                        Check if it is working locally
+                        <ol>
+                            <li>Start server (git bash or VS Code) - npm run develop</li>
+                            <li>In http://localhost:1337/admin/auth/register-admin, create a profile</li>
+                            <li>Create new collection type (Content-Types Builder)</li>
+                            <li>Check in pgAdmin was created there too: Project Database > Schemas > Public > Tables (find the name of your inserted colection in plural)</li>
+                        </ol>
+                    </li>
+                    
+                    <li>
+                        Follow this steps (VS Code): https://strapi.io/documentation/v3.x/deployment/heroku.html
+                    </li>
+
+                    <li>
+                        On production link, insert "/admin" e create a profile (this database is not the same of the development enviorement, it is needed to create a new user)
+                    </li>
+
+                    <li>Check if it is working fine
+                        <ol>
+                            <li>Check if the Collection Type created exist in production enviorment (it should exist)</li>
+                            <li>Create a new data inside this colection in Dev Envioremnt, commit the project and check (the data don't update, because are two diferents database)</li>
+                            <li>Create a new Collection type in Dev Enviorement (it is not possible do it in production anyway), commit the project and check (the Collection Type migrate do production databse)</li>
+                        </ol>                    
+                    </li>
+
+                    <li>Grab a coffee, you deserve it!</li>
+                    
+                </ol>`
+        }
+    }
+
 ]}
 
 export default CODE_TIPS_DATA;
